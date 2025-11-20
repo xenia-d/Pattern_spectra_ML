29e5ff694289ec819bd95bfc7ecd0b162ac87581
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-20 11:38:57 +0100

    fix job file

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -7,10 +7,10 @@
 #SBATCH --mem=4GB
 
 # remove all previously loaded modules
-source $HOME/venvs/HTSMvenv/bin/activate
+source $HOME/venvs/HTSMvenv_py310/bin/activate
 
 # Create working directory in TMPDIR
-mkdir $TMPDIR/pt
+mkdir $TMPDIR
 cp -r /scratch/$USER/Pattern_spectra_ML $TMPDIR
 cd $TMPDIR/Pattern_spectra_ML
 
@@ -26,7 +26,7 @@ python -u leaf_classification.py
 
 # Move Saved Files (contains plots and best model) to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
-mv "$TMPDIR/Pattern_spectra_ML/Saved Files" "/scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}/results"
+# mv "$TMPDIR/Pattern_spectra_ML/Saved Files" "/scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}/results"
 
 # Also copy SLURM output log
 cp /scratch/$USER/Pattern_spectra_ML/slurm-${SLURM_JOBID}.out /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
