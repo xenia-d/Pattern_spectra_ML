f52aa905e92a2e7a563bd2e801c6c8009b94fbca
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-20 18:41:21 +0100

    fix job file aa

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -26,7 +26,7 @@ python -u leaf_classification.py
 
 # Move Saved Files (contains plots and best model) to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
-# mv "$TMPDIR/Pattern_spectra_ML/Saved Files" "/scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}/results"
+mv "$TMPDIR/Pattern_spectra_ML/Saved_Results" "/scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}/results"
 
 # Also copy SLURM output log
 cp /scratch/$USER/Pattern_spectra_ML/slurm-${SLURM_JOBID}.out /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
