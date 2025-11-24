306255992fc4c1825ba744978e0c62d576b7607c
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-24 22:22:43 +0100

    clean up job file a bit

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -6,11 +6,9 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=4GB
 
-# remove all previously loaded modules
 source $HOME/venvs/HTSMvenv_py310/bin/activate
 
 # Create working directory in TMPDIR
-# mkdir $TMPDIR
 cp -r /scratch/$USER/Pattern_spectra_ML $TMPDIR
 cd $TMPDIR/Pattern_spectra_ML
 
@@ -20,7 +18,7 @@ mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 echo "Starting training..."
 python -u feature_extraction.py
 
-# Move Saved Files (contains plots and best model) to scratch results folder
+# Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
 mv "$TMPDIR/Pattern_spectra_ML/Saved_Results" "/scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}/results"
 
