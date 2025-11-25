6efa897b4433b6dab0c40c052aabc0588ac566f7
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-25 17:38:21 +0100

    only 3-way combos

--- feature_extraction.py
+++ feature_extraction.py
@@ -79,7 +79,7 @@ def main():
     
     # Generate all single, pairwise, and 3-channel combinations
     all_combinations = []
-    for r in range(1, len(channel_pool) + 1):
+    for r in range(1, 2, 3):
         all_combinations.extend(combinations(channel_pool, r))
 
     results = []
