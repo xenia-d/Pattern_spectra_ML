bd936c16cb5694a42b0744d509dc96696a4128bb
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-27 15:03:12 +0100

    Mondial job

--- xmaxtree/xmaxtree_bash.sh
+++ xmaxtree/xmaxtree_bash.sh
@@ -2,15 +2,15 @@
 set -e  # stop if any command fails
 
 
-OUTDIR="output/Spunta/UnhealthyLeaf_Vchannel"
+OUTDIR="output/Mondial/HealthyLeaf_vuchannel"
 mkdir -p "$OUTDIR"
 
 index1=0
 
 for i in {0..56}; do
-    for file in /mnt/c/Users/polyx/Desktop/Github\ Repos/Pattern_spectra_ML/dataset/Spunta/leaf_images/RGBFilter/channels_pgm/${i}_unhealthybox_Vpatch_*.pgm; do
+    for file in /mnt/c/Users/polyx/Desktop/Github\ Repos/Pattern_spectra_ML/dataset/Mondial/leaf_images/RGBFilter/channels_pgm/${i}_healthybox_vupatch_*.pgm; do
         base=$(basename "$file" .pgm)
-        ./xmaxtree "$file" a 9, 0 dl 1, 1 dh 5, 50 m 2, 0 n 10, 10 f 3 nogui e "$OUTDIR/uhV_${index1}_${base}"
+        ./xmaxtree "$file" a 9, 0 dl 1, 1 dh 5, 50 m 2, 0 n 10, 10 f 3 nogui e "$OUTDIR/hvu_${index1}_${base}"
         index1=$((index1 + 1))
     done
 done
