0fc5ecf8bef28f20c25a7de6a0570b36db09c673
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-28 22:11:32 +0100

    less time

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=Pattern_Spectra_Classification-Mondial
-#SBATCH --time=20:30:00
+#SBATCH --time=15:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
