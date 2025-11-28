9835baefcf819f493f6e8e9e297b4e783e076b05
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-28 11:40:03 +0100

    added result pkls to gitignore

--- xmaxtree/xmaxtree_bash.sh
+++ xmaxtree/xmaxtree_bash.sh
@@ -2,15 +2,15 @@
 set -e  # stop if any command fails
 
 
-OUTDIR="output/Rudolph/HealthyLeaf_Rchannel"
+OUTDIR="output/Rudolph/UnhealthyLeaf_vuchannel"
 mkdir -p "$OUTDIR"
 
 index1=0
 
 for i in {0..56}; do
-    for file in /mnt/c/Users/polyx/Desktop/Github\ Repos/Pattern_spectra_ML/dataset/Rudolph/leaf_images/RGBFilter/channels_pgm/${i}_healthybox_Rpatch_*.pgm; do
+    for file in /mnt/c/Users/polyx/Desktop/Github\ Repos/Pattern_spectra_ML/dataset/Rudolph/leaf_images/RGBFilter/channels_pgm/${i}_unhealthybox_vupatch_*.pgm; do
         base=$(basename "$file" .pgm)
-        ./xmaxtree "$file" a 9, 0 dl 1, 1 dh 5, 50 m 2, 0 n 10, 10 f 3 nogui e "$OUTDIR/hR_${index1}_${base}"
+        ./xmaxtree "$file" a 9, 0 dl 1, 1 dh 5, 50 m 2, 0 n 10, 10 f 3 nogui e "$OUTDIR/uhvu_${index1}_${base}"
         index1=$((index1 + 1))
     done
 done
