7774cbc1887db5e191dbb3a581338fd1c93a134b
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-28 12:04:58 +0100

    Mondial

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Pattern_Spectra_Classification-Rudolph
+#SBATCH --job-name=Pattern_Spectra_Classification-Mondial
 #SBATCH --time=20:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -16,7 +16,7 @@ cd $TMPDIR/Pattern_spectra_ML
 mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 
 echo "Starting training..."
-python -u feature_extraction.py --variant Rudolph
+python -u feature_extraction.py --variant Mondial
 
 # Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
