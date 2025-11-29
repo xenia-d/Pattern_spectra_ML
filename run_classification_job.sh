420a274714952a55594d127f40a9a0236844fcd1
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-29 23:22:47 +0100

    20 not 25 :')

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=Pattern_Spectra_Classification-Fontane
-#SBATCH --time=25:30:00
+#SBATCH --time=20:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
