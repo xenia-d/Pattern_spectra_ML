f660e5f1476aaaffa50f0d604eb9c6238a75438f
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-11-29 17:34:00 +0100

    less time

--- run_classification_job.sh
+++ run_classification_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=Pattern_Spectra_Classification_RGBHSV-Rudolph
-#SBATCH --time=20:30:00
+#SBATCH --time=3:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
