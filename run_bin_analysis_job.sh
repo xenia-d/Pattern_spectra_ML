ab3da67f12a558a2b5c61a9542710351724f6c6d
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-12-03 11:49:12 +0100

    SPUNTA

--- run_bin_analysis_job.sh
+++ run_bin_analysis_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Mondial
+#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Spunta
 #SBATCH --time=10:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -16,7 +16,7 @@ cd $TMPDIR/Pattern_spectra_ML
 mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 
 echo "Starting training..."
-python -B Feature_Extraction/run_bin_analysis.py --variant Mondial --combo R_B
+python -B Feature_Extraction/run_bin_analysis.py --variant Spunta --combo R_G_H
 
 # Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
