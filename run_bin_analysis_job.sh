64591db29ec1c9fd52f99383937eeb7f9d3fc16d
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-12-08 17:00:11 +0100

    added u instead of o

--- run_bin_analysis_job.sh
+++ run_bin_analysis_job.sh
@@ -16,7 +16,7 @@ cd $TMPDIR/Pattern_spectra_ML
 mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 
 echo "Starting training..."
-python -o Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V
+python -u Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V
 
 # Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
