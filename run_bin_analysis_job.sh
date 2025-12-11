a88ae960eadbdddf3596c1a1e539df424b377d47
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-12-11 23:00:47 +0100

    48 hr

--- run_bin_analysis_job.sh
+++ run_bin_analysis_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Fontane-deletion
-#SBATCH --time=45:10:00
+#SBATCH --time=48:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=8GB
