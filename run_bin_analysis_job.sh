24b896bc7fc01b9ebb8c028ade71ed100b4518bd
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-12-11 23:02:02 +0100

    fontane insertion

--- run_bin_analysis_job.sh
+++ run_bin_analysis_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Fontane-deletion
+#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Fontane-insertion
 #SBATCH --time=48:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=8GB
+#SBATCH --mem=4GB
 
 source $HOME/venvs/HTSMvenv_py310/bin/activate
 
@@ -15,7 +15,7 @@ cd $TMPDIR/Pattern_spectra_ML
 mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 
 echo "Starting training..."
-python -u Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V --eval_type deletion
+python -u Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V --eval_type insertion
 
 # Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
