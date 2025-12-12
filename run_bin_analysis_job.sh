9cd64244cc8ccf652b1acfd3ad397eb04a29f2b4
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-12-12 09:00:51 +0100

    del

--- run_bin_analysis_job.sh
+++ run_bin_analysis_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Fontane-insertion
+#SBATCH --job-name=Pattern_Spectra_Bin_Analysis-Fontane-deletion
 #SBATCH --time=45:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -15,7 +15,7 @@ cd $TMPDIR/Pattern_spectra_ML
 mkdir -p /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}
 
 echo "Starting training..."
-python -u Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V --eval_type insertion
+python -u Feature_Extraction/run_bin_analysis.py --variant Fontane --combo R_G_B_H_S_V --eval_type deletion
 
 # Move Saved Files to scratch results folder
 echo "Moving results to /scratch/$USER/Pattern_Spectra_ML_Results/job_${SLURM_JOBID}..."
